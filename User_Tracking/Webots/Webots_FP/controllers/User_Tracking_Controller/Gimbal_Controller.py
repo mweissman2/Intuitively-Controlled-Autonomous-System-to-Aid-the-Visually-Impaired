2bbd0e229bfa341d343c86b17a0ceadb745ec3f9
jtgrotz <jordantaylorg007@gmail.com>
2024-03-22 21:18:17 -0400

    got the motors to move, but the camera is silly

--- User_Tracking/Webots/Webots_FP/controllers/User_Tracking_Controller/Gimbal_Controller.py
+++ User_Tracking/Webots/Webots_FP/controllers/User_Tracking_Controller/Gimbal_Controller.py
@@ -1,6 +1,7 @@
 from controller import robot
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 from controller import CameraRecognitionObject
 #class for gimbal that can be instantiated multiple times
 class Gimbal_Controller:
@@ -8,67 +9,124 @@ class Gimbal_Controller:
         #set references to simulation objects
         self.rgb_camera = rgb_camera
         self.depth_camera = depth_camera
-        self.z_axis_motor = gimbal_joints[0]
-        self.y_axis_motor = gimbal_joints[1]
+        self.yaw_axis_motor = gimbal_joints[0]
+        self.pitch_axis_motor = gimbal_joints[1]
 
-        #define other initialization stuff
-        self.starting_z_axis_angle = 0
-        self.starting_y_axis_angle = 0
+        #extract camera parameters
+        self.cam_horizontal_res = float(self.rgb_camera.getWidth())
+        self.cam_vertical_res = float(self.rgb_camera.getHeight())
 
-        self.current_z_axis_angle = 0
-        self.current_y_axis_angle = 0
+        #define starting poisition of gimbal
+        self.starting_yaw_axis_angle = 0
+        self.starting_pitch_axis_angle = 0
+
+        #define set point for user tracking, normalized
+        self.x_setpoint = float(self.cam_horizontal_res/2)
+        self.y_setpoint = float(self.cam_vertical_res/2)
+
+        #saves state of last main direction to rotate that way if person is lost.
+        self.last_direction = -1
+        self.user_in_frame = False
+        self.center_error_margin = 0.08 #50 pixels?
+
+        self.current_yaw_axis_angle = 0
+        self.current_pitch_axis_angle = 0
+        self.yaw_pid = [0.15, 0.01, 0]
+        self.yaw_pid_errors = [0,0] #last error, error sum
+        self.pitch_pid = [0.15 , 0, 0]
+        self.pitch_pid_errors = [0, 0]  #last error, error sum
         self.update_time = update_time
         self.user_id = 30
 
 
     def set_user_id(self, user_id):
         self.user_id = user_id
-    def enable_segmentation(self, status):
-        if status == False:
-            #disable segmentation
-            self.rgb_camera.disableRecognitionSegmentation()
-        else:
-            #enable segmentation
-            self.rgb_camera.enableRecognitionSegmentation()
 
-    def enable_recognition(self, status):
-        if status == False:
-            #disable recognition
-            self.rgb_camera.recognitionDisable()
-        else:
-            #enable recognition
-            self.rgb_camera.recognitionEnable(self.update_time)
+    def center_user(self, user_position_in_image, good_frame):
+        #normalize inputs
+        x_input = (float(user_position_in_image[0])/self.cam_horizontal_res) - 0.5
+        y_input = (float(user_position_in_image[1])/self.cam_vertical_res) - 0.5
+        #normalize setpoints
+        xn_setpoint = (self.x_setpoint/self.cam_horizontal_res) - 0.5
+        yn_setpoint = (self.y_setpoint / self.cam_vertical_res) - 0.5
 
-    def center_user(self):
-        return 0
+
+        yaw_pos_diff, self.yaw_pid_errors[0], self.yaw_pid_errors[1] = self.calc_pid(xn_setpoint, x_input, self.yaw_pid, self.yaw_pid_errors[0], self.yaw_pid_errors[1])
+        self.yaw_axis_motor.setPosition(self.current_yaw_axis_angle+yaw_pos_diff)
+        pitch_pos_diff, self.pitch_pid_errors[0], self.pitch_pid_errors[1] = self.calc_pid(yn_setpoint, y_input, self.pitch_pid, self.pitch_pid_errors[0], self.pitch_pid_errors[1])
+        self.pitch_axis_motor.setPosition(self.current_pitch_axis_angle+pitch_pos_diff)
+
+        #if within an error threshold for image centering, #then calculate depth and angle
+        if np.abs(xn_setpoint-x_input) < self.center_error_margin:
+            #calculate depth
+            self.calculate_depth(good_frame)
+            #calculate angle
+            #calculate position from robot
+            #update velocity estimator
+            #update update state estimator
+
+
+
     def find_user(self):
+        self.update_sensor_readings()
+        good_frame = []
         objects = self.rgb_camera.getRecognitionObjects()
+        #assumes the user is not in Frame
+        self.user_in_frame = False
         for obj in objects:
             if obj.getId() == self.user_id:
+                #if they are found then set boolean to true
+                self.user_in_frame = True
                 position = obj.getPosition()
                 position_on_image = obj.getPositionOnImage()
-                print(position_on_image[0])
+                #TODO maybe change to just sending the image, or the segmentation?
+                my_frame = self.rgb_camera.getImageArray()
+                good_frame = my_frame.copy()
+                #print(position_on_image[0])
 
-    def enable_vision(self, status):
-        self.enable_recognition(status)
-        self.enable_segmentation(status)
+        if self.user_in_frame == True:
+            #call the center user function
+            self.center_user(position_on_image, good_frame)
+        else:
+            #else do continuous rotation to find user
+            #calculate new angle and set
+            new_angle = self.current_yaw_axis_angle + 0.35
+            self.yaw_axis_motor.setPosition(new_angle)
 
     def process_image(self, image):
         return 0
 
     def segment_image(self, image):
-        return 0
+        #image_c_ptr = self.rgb_camera.getRecognitionSegmentationImage()
+        #image_np = np.ctypeslib.as_array(image_c_ptr, (self.rgb_camera.getWidth() * self.rgb_camera.getHeight(),))
+        image_np = self.rgb_camera.getRecognitionSegmentationImageArray()
+        #plt.imshow(image)
+        #plt.show()
+        #cv.waitKey(0)
+        return image_np
 
     def calculate_depth(self,depth_image):
-        return 0
+        depth = -1
+        #get segmentation from RGB image
+        mask = self.segment_image(depth_image)
+        #get depth image
+        #apply mask to depth image
+        #average together readings from depth image
+        return depth
 
     def get_depth_image(self):
         image_c_ptr = self.depth_camera.getRangeImage(data_type="buffer")
         image_np = np.ctypeslib.as_array(image_c_ptr, (self.depth_camera.getWidth() * self.depth_camera.getHeight(),))
         return image_np
 
+    #function for reading out the angle of the position sensor on the robot.
     def calculate_angle(self):
-        return 0
+        yaw_angle = self.yaw_axis_motor.getPositionSensor().getValue()
+        return yaw_angle
+
+    def update_sensor_readings(self):
+        self.current_yaw_axis_angle = self.yaw_axis_motor.getPositionSensor().getValue()
+        self.current_pitch_axis_angle = self.pitch_axis_motor.getPositionSensor().getValue()
 
     def calculate_user_position(self):
         return 0
@@ -79,5 +137,37 @@ class Gimbal_Controller:
     def estimate_next_position(self):
         return 0
 
+    #setter function for enabling segmentation (needs to also be set in webot world)
+    def enable_segmentation(self, status):
+        if status == False:
+            #disable segmentation
+            self.rgb_camera.disableRecognitionSegmentation()
+        else:
+            #enable segmentation
+            self.rgb_camera.enableRecognitionSegmentation()
 
+    #setter function for enabling recognition
+    def enable_recognition(self, status):
+        if status == False:
+            #disable recognition
+            self.rgb_camera.recognitionDisable()
+        else:
+            #enable recognition
+            self.rgb_camera.recognitionEnable(self.update_time)
 
+    # setter function for totally enabling the computer vision pipeline.
+    def enable_tracking(self, status):
+        self.enable_recognition(status)
+        self.enable_segmentation(status)
+
+        self.yaw_axis_motor.getPositionSensor().enable(32)
+        self.pitch_axis_motor.getPositionSensor().enable(32)
+
+    def calc_pid(self,set_point, current_point, kpid, last_error, error_sum):
+        error = set_point - current_point
+        p_error = kpid[0]*error
+        error_sum += error
+        i_error = kpid[1]*error_sum
+        d_error = kpid[2]*(error - last_error)
+        output = p_error + i_error + d_error
+        return output, error, error_sum
--- User_Tracking/Webots/Webots_FP/controllers/User_Tracking_Controller/User_Tracking_Controller.py
+++ User_Tracking/Webots/Webots_FP/controllers/User_Tracking_Controller/User_Tracking_Controller.py
@@ -3,14 +3,21 @@
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
+from controller import Supervisor
 import Gimbal_Controller as GC
 
+robot = Supervisor()
+#get ID of user for tracking.
+user_node = robot.getFromDef('USER')
+user_id = user_node.getId()
 # create the Robot instance.
-robot = Robot()
+#my_robot = robot.getFromDef('DOG')
+
+
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
-camera_timestep = 33
+camera_timestep = 32
 
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
@@ -31,19 +38,22 @@ joint_names = ['gimbal_j1_motor', 'gimbal_j2_motor']
 for names in joint_names:
     gimbal_joints.append(robot.getDevice(names))
     
-    
-#set robot joints
-gimbal_joints[0].setPosition(1.3)
-gimbal_joints[1].setPosition(0.4)  
+
 
 tracker = GC.Gimbal_Controller(rgb_camera, depth_camera, gimbal_joints,camera_timestep)
-tracker.enable_vision(True)
+#enables computer vision and sets the ID for user recognition
+tracker.enable_tracking(True)
+tracker.set_user_id(user_id)
 print("Tracking started")
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
+i = 0
 while robot.step(timestep) != -1:
     # Read the sensors:
-    tracker.get_depth_image()
+    if i == 3:
+        tracker.find_user()
+        i = 0
+    i += 1
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
 
