01f949209ed65395f26ca3d160ea1fd34d8f1348
jtgrotz <jordantaylorg007@gmail.com>
2024-03-26 20:53:03 -0400

    finished implementation, need to tune and fix accuracy

--- User_Tracking/Webots/Webots_FP/controllers/User_Tracking_Controller/Gimbal_Controller.py
+++ User_Tracking/Webots/Webots_FP/controllers/User_Tracking_Controller/Gimbal_Controller.py
@@ -1,3 +1,5 @@
+import math
+
 from controller import robot
 import cv2 as cv
 import numpy as np
@@ -25,15 +27,20 @@ class Gimbal_Controller:
         self.y_setpoint = float(self.cam_vertical_res/2)
 
         #saves state of last main direction to rotate that way if person is lost.
-        self.last_direction = -1
+        self.last_direction = -1 #positive 1 for positive rotation, -1 for negative rotation
         self.user_in_frame = False
-        self.center_error_margin = 0.08 #50 pixels?
+        self.center_error_margin = 0.06 #50 pixels?
+
+        self.x_velocity_buffer = [0,0,0,0,0]
+        self.y_velocity_buffer = [0,0,0,0,0]
+        self.velocity_buffer_index = 0
+        self.prev_position = [-2,0]
 
         self.current_yaw_axis_angle = 0
         self.current_pitch_axis_angle = 0
-        self.yaw_pid = [0.15, 0.01, 0]
+        self.yaw_pid = [0.35, 0.03, 0]
         self.yaw_pid_errors = [0,0] #last error, error sum
-        self.pitch_pid = [0.15 , 0, 0]
+        self.pitch_pid = [0.35 , 0, 0]
         self.pitch_pid_errors = [0, 0]  #last error, error sum
         self.update_time = update_time
         self.user_id = 30
@@ -56,15 +63,30 @@ class Gimbal_Controller:
         pitch_pos_diff, self.pitch_pid_errors[0], self.pitch_pid_errors[1] = self.calc_pid(yn_setpoint, y_input, self.pitch_pid, self.pitch_pid_errors[0], self.pitch_pid_errors[1])
         self.pitch_axis_motor.setPosition(self.current_pitch_axis_angle+pitch_pos_diff)
 
+
+        #determine last direction
+        if pitch_pos_diff > 0:
+            self.last_direction = 1
+        else:
+            self.last_direction = -1
         #if within an error threshold for image centering, #then calculate depth and angle
         if np.abs(xn_setpoint-x_input) < self.center_error_margin:
-            #calculate depth
-            self.calculate_depth(good_frame)
-            #calculate angle
-            #calculate position from robot
-            #update velocity estimator
-            #update update state estimator
+            self.get_user_location(good_frame)
 
+
+    def get_user_location(self, good_frame):
+        #calculate depth
+        depth = self.calculate_depth(good_frame)
+        #calculate angle
+        angle = self.calculate_angle()
+        #calculate position from robot
+        curr_position = self.calculate_user_position(depth, angle)
+        #update velocity estimator
+        curr_velocity = self.interpolate_velocity(curr_position)
+        #update state estimator
+        self.estimate_next_position(curr_position,curr_velocity)
+
+        return (curr_position, curr_velocity)
 
 
     def find_user(self):
@@ -82,19 +104,15 @@ class Gimbal_Controller:
                 #TODO maybe change to just sending the image, or the segmentation?
                 my_frame = self.rgb_camera.getImageArray()
                 good_frame = my_frame.copy()
-                #print(position_on_image[0])
 
         if self.user_in_frame == True:
             #call the center user function
             self.center_user(position_on_image, good_frame)
         else:
-            #else do continuous rotation to find user
+            #else do continuous rotation in the same as the previous direction to find user
             #calculate new angle and set
-            new_angle = self.current_yaw_axis_angle + 0.35
+            new_angle = self.current_yaw_axis_angle + (self.last_direction*0.35)
             self.yaw_axis_motor.setPosition(new_angle)
-
-    def process_image(self, image):
-        return 0
 
     def segment_image(self, image):
         #remove infinity from array
@@ -120,9 +138,9 @@ class Gimbal_Controller:
         masked_image, pixel_count = self.segment_image(depth_image)
 
         #make into 1d array for easier processing
-        #determine how many indicies have a
         flattened_image = masked_image.flatten()
         depth = np.sum(flattened_image, where=flattened_image>0)/pixel_count
+        #print(depth)
         #average together readings from depth image
         return depth
 
@@ -140,14 +158,42 @@ class Gimbal_Controller:
         self.current_yaw_axis_angle = self.yaw_axis_motor.getPositionSensor().getValue()
         self.current_pitch_axis_angle = self.pitch_axis_motor.getPositionSensor().getValue()
 
-    def calculate_user_position(self):
-        return 0
+    def calculate_user_position(self, depth, angle):
+        x_pos = depth*math.cos(angle)
+        y_pos = depth*math.sin(angle)
 
-    def interpolate_velocity(self):
-        return 0
+        return x_pos, y_pos
 
-    def estimate_next_position(self):
-        return 0
+    def interpolate_velocity(self, curr_position):
+        #calculate current velocity
+        curr_x_velocity = (curr_position[0]-self.prev_position[0])/(self.update_time/1000)
+        curr_y_velocity = (curr_position[1] - self.prev_position[1]) / (self.update_time/1000)
+
+        self.prev_position = curr_position
+
+        #push to buffer
+        self.x_velocity_buffer[self.velocity_buffer_index] = curr_x_velocity
+        self.y_velocity_buffer[self.velocity_buffer_index] = curr_y_velocity
+
+        #implement circular buffer
+        self.velocity_buffer_index += 1
+        if self.velocity_buffer_index >= len(self.x_velocity_buffer):
+            self.velocity_buffer_index = 0
+
+        #average buffer
+        x_vel = np.average(self.x_velocity_buffer)
+        y_vel = np.average(self.y_velocity_buffer)
+        #print(x_vel, y_vel)
+
+        return x_vel, y_vel
+
+    def estimate_next_position(self, position, velocity):
+        next_x_pos = position[0]+velocity[0]*self.update_time
+        next_y_pos = position[1]+velocity[1]*self.update_time
+
+        self.next_position = [next_x_pos, next_y_pos]
+
+        return (next_x_pos, next_y_pos)
 
     #setter function for enabling segmentation (needs to also be set in webot world)
     def enable_segmentation(self, status):
